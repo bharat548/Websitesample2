7dceb8fca9f8efd22dd0ceb7b9b367e1f990bb8d
bharat sharma <sharmab07@gmail.com>
2020-05-09 12:55:34 -0500

    adding

---
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    class ProductRepository
+    {
+        Objectcache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+        public ProductRepository()
+        {
+            products = cache("products") as List<Product>;
+            if (products = null)
+            {
+                products = new List<Product>();
+
+            }
+        }
+
+        private List<Product> cache(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
